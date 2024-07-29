cadeba4914c759949808fe0296a730b4e483d38a
Pierre Peinado <pierre.peinado@webinage.fr>
2024-07-29 22:59:39 +0200

    update pino conf for production

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,13 +1,16 @@
 import pino, { Logger } from "pino";
 
-export const logger: Logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-    },
-  },
-  level: "info",
+export const logger: Logger =
+  process.env["NODE_ENV"] === "production"
+    ? pino({ level: "WARN" })
+    : pino({
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+          },
+        },
+        level: "debug",
 
-  redact: [], // prevent logging of sensitive data
-});
+        redact: [], // prevent logging of sensitive data
+      });
